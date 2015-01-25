8e0e8b08811427eec3adec854081cac290222d2b
Venkat Veera <vveera9@gmail.com>
2015-01-25 12:03:33 -0600

    CacheMatrix
    Updating the header

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,4 @@
+# Coursera Data Science R Programming - Week 3 assignment
 # Matrix inversion is usually a costly computation and there may be some benefit to 
 # caching the inverse of a matrix rather than computing it repeatedly 
 # makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
